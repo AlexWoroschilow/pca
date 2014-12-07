7810d4ea705629a373afb67e8d42101b857cebc8
Alex Woroschilow <alex.woroschilow@gmail.com>
2014-12-07 18:25:54 +0100

    change to module

---
+++ compare.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use Salami::Compare;
+
+use POSIX qw(EXIT_SUCCESS EXIT_FAILURE);
+
+
+sub mymain () {
+  use Getopt::Std;
+
+  my $proteins_ref  = $ARGV[0];
+  my $proteins_test = $ARGV[1];
+
+  test( $proteins_ref, $proteins_test );
+}
+
+exit( mymain() );
