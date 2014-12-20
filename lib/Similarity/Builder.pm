48e4702d5d4b58e110ecbbe3532a986c0529e22e
Alex Woroschilow <alex.woroschilow@gmail.com>
2014-12-20 20:08:03 +0100

    update lib

--- lib/Similarity/Builder.pm
+++ lib/Similarity/Builder.pm
@@ -2,20 +2,17 @@ package Similarity::Builder;
 use FindBin;
 use base Exporter;
 use Text::Xslate;
-
 use lib "$FindBin::Bin/lib/";
 use Similarity::Pca;
 use Similarity::Compare;
 
 sub new () {
   my ( $class, $args ) = @_;
-  
   my $self = {
     ref1 => $args->{ref1},
     ref2 => $args->{ref2},
     pcc  => $args->{pcc}
   };
-  
   return bless $self, $class;
 }
 
@@ -23,31 +20,32 @@ sub new () {
 # get a result as a multidimensional array
 sub all_to_all () {
   my $self = shift;
+  my @ref1 = @{ $self->{ref1} };
+  my @ref2 = @{ $self->{ref2} };
 
   # Use script from Andrew Torda
-  my (@matrix) = Similarity::Compare::all_to_all( 
-    @{$self->{ref1}}, @{$self->{ref2}} 
-  );
+  my @matrix = Similarity::Compare::all_to_all( @ref1, @ref2 );
 
   # Apply PCA to given matrix
   # return all matrixes (raw, rest, P, T)
-  $pca = new Similarity::Pca({
+  my $pca = new Similarity::Pca(
+    {
       matrix => \@matrix,
       pc     => $this->{pcc}
-    });
-    
+    }
+  );
   if ( $pca->pca() ) {
-#    $pca->{r}->print("-->Raw:\n");
-#    $pca->{m}->print("-->Rest:\n");
-#    $pca->{p}->print("Matrix P: \n");
-#    $pca->{t}->print("Matrix T: \n");
 
+    #    $pca->{r}->print("-->Raw:\n");
+    #    $pca->{m}->print("-->Rest:\n");
+    #    $pca->{p}->print("Matrix P: \n");
+    #    $pca->{t}->print("Matrix T: \n");
     # Build a data structure
     # to push into templater
     my $result = {};
     my ( $m, $n ) = $pca->{t}->size;
     foreach my $i ( 0 ... ( $m - 1 ) ) {
-      $result->{"@ref[$i]"} = {
+      $result->{"@ref1[$i]"} = {
         "x" => $pca->{t}->[$i]->[0],
         "y" => $pca->{t}->[$i]->[1],
         "z" => $pca->{t}->[$i]->[2],
